b71df17a5e6a6565f0caa587c4a29a10cda4c44d
Vincent Fiszbin <fiszbin.vincent@gmail.com>
2022-07-24 12:40:16 +0200

    Norme init.c

--- init.c
+++ init.c
@@ -6,16 +6,21 @@
 /*   By: vfiszbin <vfiszbin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/30 11:08:20 by vfiszbin          #+#    #+#             */
-/*   Updated: 2022/07/22 19:09:56 by vfiszbin         ###   ########.fr       */
+/*   Updated: 2022/07/24 12:15:28 by vfiszbin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cu3d.h"
 
+/**
+ * @brief Initialize members of the vars struct
+ * 
+ * @param vars 
+ */
 void	init_vars_struct(t_vars *vars)
 {
-	int i;
-	
+	int	i;
+
 	vars->mlx = NULL;
 	vars->win = NULL;
 	vars->img1.img = NULL;
@@ -26,12 +31,10 @@ void	init_vars_struct(t_vars *vars)
 		vars->texture[i].img = NULL;
 		i++;
 	}
-	//x and y start position
 	vars->posX = 2;
 	vars->posY = 4;
-	vars->moveSpeed = 0.05; //squares/second
-	vars->rotSpeed = 0.03; //radians/second
-	
+	vars->moveSpeed = 0.05;
+	vars->rotSpeed = 0.03;
 	vars->move_forward = 0;
 	vars->move_backward = 0;
 	vars->move_left = 0;
@@ -40,7 +43,12 @@ void	init_vars_struct(t_vars *vars)
 	vars->rotate_right = 0;
 }
 
-void init_mlx_and_img_buffers(t_vars *vars)
+/**
+ * @brief Initialize mlx related variables, including images
+ * 
+ * @param vars 
+ */
+void	init_mlx_and_img_buffers(t_vars *vars)
 {
 	vars->mlx = mlx_init();
 	if (vars->mlx == NULL)
@@ -51,15 +59,37 @@ void init_mlx_and_img_buffers(t_vars *vars)
 	vars->img1.img = mlx_new_image(vars->mlx, screenWidth, screenHeight);
 	if (vars->img1.img == NULL)
 		close_program(vars, 1);
-	vars->img1.addr = mlx_get_data_addr(vars->img1.img, &vars->img1.bits_per_pixel, &vars->img1.line_length, &vars->img1.endian);
+	vars->img1.addr = mlx_get_data_addr(vars->img1.img,
+			&vars->img1.bits_per_pixel, &vars->img1.line_length,
+			&vars->img1.endian);
 	if (vars->img1.addr == NULL)
 		close_program(vars, 1);
 	vars->img2.img = mlx_new_image(vars->mlx, screenWidth, screenHeight);
 	if (vars->img2.img == NULL)
 		close_program(vars, 1);
-	vars->img2.addr = mlx_get_data_addr(vars->img2.img, &vars->img2.bits_per_pixel, &vars->img2.line_length, &vars->img2.endian);
+	vars->img2.addr = mlx_get_data_addr(vars->img2.img,
+			&vars->img2.bits_per_pixel, &vars->img2.line_length,
+			&vars->img2.endian);
 	if (vars->img2.addr == NULL)
 		close_program(vars, 1);
+}
+
+void	set_init_direction2(t_vars *vars, char dir)
+{
+	if (dir == 'E')
+	{
+		vars->dirX = 0;
+		vars->dirY = 1;
+		vars->planeX = 0.66;
+		vars->planeY = 0;
+	}
+	else if (dir == 'W')
+	{
+		vars->dirX = 0;
+		vars->dirY = -1;
+		vars->planeX = -0.66;
+		vars->planeY = 0;
+	}
 }
 
 /**
@@ -68,36 +98,22 @@ void init_mlx_and_img_buffers(t_vars *vars)
  * @param vars 
  * @param dir N,S,E or W
  */
-void set_init_direction(t_vars *vars, char dir)
+void	set_init_direction(t_vars *vars, char dir)
 {
 	if (dir == 'N')
 	{
-		//initial direction vector
 		vars->dirX = -1;
-		vars->dirY = 0; 
-		//the 2d raycaster version of camera plane
+		vars->dirY = 0;
 		vars->planeX = 0;
-		vars->planeY = 0.66; 
+		vars->planeY = 0.66;
 	}
 	else if (dir == 'S')
 	{
 		vars->dirX = 1;
-		vars->dirY = 0; 
+		vars->dirY = 0;
 		vars->planeX = 0;
-		vars->planeY = -0.66; 
+		vars->planeY = -0.66;
 	}
-	else if (dir == 'E')
-	{
-		vars->dirX = 0;
-		vars->dirY = 1; 
-		vars->planeX = 0.66;
-		vars->planeY = 0; 
-	}
-	else if (dir == 'W')
-	{
-		vars->dirX = 0;
-		vars->dirY = -1; 
-		vars->planeX = -0.66;
-		vars->planeY = 0; 
-	}
+	else
+		set_init_direction2(vars, dir);
 }
